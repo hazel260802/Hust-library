6ded6e2f5e199c1f161e04f80b9b319f20a468e9
hazel260802 <87821296+hazel260802@users.noreply.github.com>
2022-07-21 21:39:56 +0700

    Delete library directory

--- library/fontend/form.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import Length, DataRequired, ValidationError
-from .models import Sach
-from markupsafe import Markup
-class SearchForm(FlaskForm):
-    search = StringField('search', validators=[DataRequired(), Length(max=100)])
-    search_icon = Markup ("<i class=\"fa fa-search\"></i>")
-    search_button = SubmitField(search_icon, id="symbolBtn")
